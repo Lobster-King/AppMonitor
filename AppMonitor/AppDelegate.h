76a99ebae92b583dfd702d45f54a3f067f9368fd
Lobster-King <win_cooperation@163.com>
2016-06-03 10:08:18 +0800

    project init
    project init

---
+++ AppMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppMonitor
+//
+//  Created by qinzhiwei on 16/6/3.
+//  Copyright © 2016年 AppMonitor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppMonitor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppMonitor
+//
+//  Created by qinzhiwei on 16/6/3.
+//  Copyright © 2016年 AppMonitor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
